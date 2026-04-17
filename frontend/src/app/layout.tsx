a63350d8d6b31db5fc615b225165f4525cecf3b5
Steven Alvin Christian <5025241116@student.its.ac.id>
2026-04-17 20:18:49 +0700

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,9 +4,6 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "VISIONGUARD",
   description: "VISIONGUARD: Mengungkap Kegagalan Persepsi Model Deteksi pada Kendaraan Otonom dengan Explainable AI",
-  icons: {
-    icon: "/favicon.ico",
-  },
 };
 
 export default function RootLayout({
